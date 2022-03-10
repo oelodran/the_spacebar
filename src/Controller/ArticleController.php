527bf1f0c5716d9034c2466dd25b5c4aaac63569
Leonardo Ramljak <leonardo.ramljak@gmail.com>
2022-03-10 21:48:37 +0100

    chapter 06

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/", name="homepage")
  */
-class ArticleController
+class ArticleController extends AbstractController
 {
     public function homepage(): Response
     {
@@ -20,6 +21,15 @@ class ArticleController
      */
     public function show($slug): Response
     {
-        return new Response(printf('Future page to show article: %s', $slug));
+        $comments = [
+            'I ate a normal rock once. It did NOT taste lake bacon!',
+            'I \' m going on an all-astroid diet!',
+            'I like bacon too. Buy some form my site. bacinsombacon.com'
+        ];
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments
+        ]);
     }
 }
