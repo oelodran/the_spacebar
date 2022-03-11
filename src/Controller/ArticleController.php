ea12ee7becbf674345641baa94abd3feb495af47
Leonardo Ramljak <leonardo.ramljak@gmail.com>
2022-03-11 12:50:35 +0100

    Finish chapter 12

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,34 +2,50 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ArticleController extends AbstractController
 {
     /**
      * @Route("/", name="app_homepage")
      */
-    public function homepage(): Response
+    public function homepage()
     {
-       return $this->render('article/homepage.html.twig');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug): Response
+    public function show($slug)
     {
         $comments = [
-            'I ate a normal rock once. It did NOT taste lake bacon!',
-            'I \' m going on an all-astroid diet!',
-            'I like bacon too. Buy some form my site. bacinsombacon.com'
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
-            'comments' => $comments
+            'slug' => $slug,
+            'comments' => $comments,
         ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug, LoggerInterface $logger)
+    {
+        // TODO - actually heart/unheart the article!
+
+        $logger->info('Article is being hearted!');
+
+        return new JsonResponse(['hearts' => rand(5, 100)]);
     }
 }
